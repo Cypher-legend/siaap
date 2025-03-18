960ea4d6faf61c98667ca8a64ed2ed33a983a696
Siddharth <siddhudk2@gmail.com>
2025-03-18 11:34:21 +0530

    revert mongodb changes

--- server/server.js
+++ server/server.js
@@ -45,4 +45,4 @@ app.post('/api/staff/login', async (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log("Server running on port ${PORT}"));
